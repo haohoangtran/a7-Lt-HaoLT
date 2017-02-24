2e54982b0274e8c858f3f43fcc8c05a727be9abf
DESKTOP-4AAPR0I\tranh <tranhaockpt@gmail.com>
2017-02-24 15:02:07 +0700

    add edit,delete,add task

---
+++ Day 10/app/src/main/java/com/example/tranh/pomodoro/networks/services/RegisterService.java
@@ -0,0 +1,17 @@
+package com.example.tranh.pomodoro.networks.services;
+
+import com.example.tranh.pomodoro.networks.jsonmodels.RegisterResponseJSON;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * Created by tranh on 1/19/2017.
+ */
+
+public interface RegisterService {
+
+
+}
